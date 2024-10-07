2aca9411d89fbba9bec917877514062756012ca7
202210416 <157572769+AllenjakeLavina@users.noreply.github.com>
2024-10-07 22:51:58 +0800

    Google Font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,12 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+    import "../app.css";
+</script>
+
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</svelte:head>
+
+<slot></slot>
